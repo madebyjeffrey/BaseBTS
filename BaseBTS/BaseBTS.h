14741925fdac172af02af3a9b2cec553240e3e2b
Jeffrey Drake <jeff@ideaplex.ca>
2024-04-24 00:32:16 -0600

    Initial Commit

---
+++ BaseBTS/BaseBTS.h
@@ -0,0 +1,18 @@
+//
+//  BaseBTS.h
+//  BaseBTS
+//
+//  Created by Jeffrey Drake on 2024-04-24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseBTS.
+FOUNDATION_EXPORT double BaseBTSVersionNumber;
+
+//! Project version string for BaseBTS.
+FOUNDATION_EXPORT const unsigned char BaseBTSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseBTS/PublicHeader.h>
+
+
